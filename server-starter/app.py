82f4f0d753fd66b7a3eebe58b1aafd446d2d426f
Niels Voss <nvosscode@gmail.com>
2021-02-15 11:51:36 -0800

    Make Server Starter function retrieve server data

--- server-starter/app.py
+++ server-starter/app.py
@@ -1,4 +1,26 @@
+import json
+import boto3
+import os
+
 def lambda_handler(event, context):
+    id = event['Id']
+    state = event['Server_State']
+
+    # Make sure server is offline
+    assert state in ['SERVER_SHUTDOWN_FUNCTION_CALLED', 'SERVER_STOPPING', 'SERVER_OFFLINE', ''], f"Server state {state} is not offline"
+
+    table = boto3.resource('dynamodb').Table(os.environ['DATABASE_NAME'])
+    print(os.environ['DATABASE_NAME'])
+
+    # Get server data
+    data = table.get_item(
+        Key={
+            'Id': id
+        },
+        ProjectionExpression='EBS, EC2Instance, EC2SpotRequest',
+        ConsistentRead=True
+    )['Item']
+
     return {
         "statusCode": 200,
         "body": "hello world"
