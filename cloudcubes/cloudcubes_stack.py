bde413aa828bb488d1ad90c38c034e9458c7bb77
Niels Voss <nvosscode@gmail.com>
2021-02-25 22:30:13 -0800

    Create database name parameter

--- cloudcubes/cloudcubes_stack.py
+++ cloudcubes/cloudcubes_stack.py
@@ -8,9 +8,17 @@ class CloudcubesStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        db_name = cdk.CfnParameter(self, "Database_Name",
+            type="String",
+            description="The name of the dynamodb database where the information on server data is stored."
+        )
+
         # The code that defines your stack goes here
         lambda_.Function(self, "SchedulerFunction",
             runtime=lambda_.Runtime.PYTHON_3_8,
             code=lambda_.Code.from_asset("scheduler"),
-            handler="app.lambda_handler"
+            handler="app.lambda_handler",
+            environment={
+                "DATABASE_NAME": db_name.value_as_string
+            }
         )
