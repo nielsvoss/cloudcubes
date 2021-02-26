5322b19830ef53540415c403dc9b750f6bfe80ab
Niels Voss <nvosscode@gmail.com>
2021-02-25 22:04:38 -0800

    Start creating lambda functions in cdk stack

--- cloudcubes/cloudcubes_stack.py
+++ cloudcubes/cloudcubes_stack.py
@@ -1,9 +1,16 @@
-from aws_cdk import core
+import aws_cdk.core as cdk
+import aws_cdk.aws_s3 as s3
+import aws_cdk.aws_lambda as lambda_
 
 
-class CloudcubesStack(core.Stack):
+class CloudcubesStack(cdk.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        lambda_.Function(self, "SchedulerFunction",
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            code=lambda_.Code.from_asset("scheduler"),
+            handler="app.lambda_handler"
+        )
