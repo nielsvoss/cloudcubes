cb04e8ac8bf663fbb6aa87aae076832e80a695f3
Niels Voss <nvosscode@gmail.com>
2021-02-26 15:19:35 -0800

    Start creating scripts bucket

--- cloudcubes/cloudcubes_stack.py
+++ cloudcubes/cloudcubes_stack.py
@@ -9,11 +9,13 @@ class CloudcubesStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Name of the DynamoDB database
         db_name = cdk.CfnParameter(self, "Database_Name",
             type="String",
             description="The name of the dynamodb database where the information on server data is stored."
         )
 
+        # Grants access to the dynamodb database
         database_perms = iam.PolicyStatement(
             effect=iam.Effect.ALLOW,
             resources=[f'arn:aws:dynamodb:*:*:table/{db_name.value_as_string}'],
@@ -31,14 +33,25 @@ class CloudcubesStack(cdk.Stack):
             ]
         )
 
-        # The code that defines your stack goes here
+        # The lambda function in charge of scheduling servers
         scheduler_function = lambda_.Function(self, "SchedulerFunction",
             runtime=lambda_.Runtime.PYTHON_3_8,
-            code=lambda_.Code.from_asset("scheduler"),
+            code=lambda_.Code.from_asset("./scheduler"),
             handler="app.lambda_handler",
             environment={
                 "DATABASE_NAME": db_name.value_as_string
             }
         )
-
         scheduler_function.add_to_role_policy(database_perms)
+
+        # S3 bucket to store scripts (used by EC2 instances)
+        scripts_bucket = s3.Bucket(self, "ScriptsBucket",
+            bucket_name='cloudcubes-scripts-bucket',
+            versioned=False,
+            removal_policy=cdk.RemovalPolicy.DESTROY
+        )
+
+        scripts_bucket_deployment = s3_deployment.BucketDeployment(self, "ScriptsBucketDeployment",
+            destination_bucket=scripts_bucket,
+            sources=[s3_deployment.Source.asset('./scripts')]
+        )
