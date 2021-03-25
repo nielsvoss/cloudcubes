3726bf0045f8fd20f10f373d03a0fcd4db0a1b01
Niels Voss <nvosscode@gmail.com>
2021-03-25 00:02:37 -0700

    Store data bucket name in file

--- resources/templates/paper-server/backup.sh
+++ resources/templates/paper-server/backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+data_bucket_name=$(cat /home/ec2-user/info/DATA_BUCKET_NAME)
 name=$(date +%Y.%m.%d.%H.%M.%S)
 
 cd /home/ec2-user
 sudo zip -r backup.zip server/minecraft
-aws s3 cp backup.zip s3://$1/backups/$name.zip
+aws s3 cp backup.zip s3://$data_bucket_name/backups/$name.zip
 sudo rm backup.zip
--- resources/templates/paper-server/prepare.sh
+++ resources/templates/paper-server/prepare.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Write data bucket name to file
+mkdir /home/ec2-user/info
+echo $DATA_BUCKET > /home/ec2-user/info/DATA_BUCKET_NAME
 # Add cron job to crontab
-(crontab -l 2>/dev/null; echo "0 22 * * * /home/ec2-user/server/backup.sh $DATA_BUCKET") | crontab -
+(crontab -l 2>/dev/null; echo "0 22 * * * /home/ec2-user/server/backup.sh") | crontab -
