5ae21aac7eea671798543e9dde5d27c33e73aef8
Niels Voss <nvosscode@gmail.com>
2021-03-25 00:27:45 -0700

    Implement shifting backup system

--- resources/templates/paper-server/backup.sh
+++ resources/templates/paper-server/backup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 data_bucket_name=$(cat /home/ec2-user/info/DATA_BUCKET_NAME)
-name=$(date +%Y.%m.%d.%H.%M.%S)
+
+if [ "$1" = "shifting" ]; then
+  aws s3 rm s3://$data_bucket_name/backups/backup-5.zip
+  aws s3 mv s3://$data_bucket_name/backups/backup-4.zip s3://$data_bucket_name/backups/backup-5.zip
+  aws s3 mv s3://$data_bucket_name/backups/backup-3.zip s3://$data_bucket_name/backups/backup-4.zip
+  aws s3 mv s3://$data_bucket_name/backups/backup-2.zip s3://$data_bucket_name/backups/backup-3.zip
+  aws s3 mv s3://$data_bucket_name/backups/backup-1.zip s3://$data_bucket_name/backups/backup-2.zip
+  aws s3 mv s3://$data_bucket_name/backups/latest-backup.zip s3://$data_bucket_name/backups/backup-1.zip
+  name="latest-backup"
+else
+  name=$(date +%Y.%m.%d.%H.%M.%S)
+fi
 
 cd /home/ec2-user
 sudo zip -r backup.zip server/minecraft
--- resources/templates/paper-server/prepare.sh
+++ resources/templates/paper-server/prepare.sh
@@ -4,3 +4,4 @@ mkdir /home/ec2-user/info
 echo $DATA_BUCKET > /home/ec2-user/info/DATA_BUCKET_NAME
 # Add cron job to crontab
 (crontab -l 2>/dev/null; echo "0 22 * * * /home/ec2-user/server/backup.sh") | crontab -
+(crontab -l 2>/dev/null; echo "0 */4 * * * /home/ec2-user/server/backup.sh shifting") | crontab -
