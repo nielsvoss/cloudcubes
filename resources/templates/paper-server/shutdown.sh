f3328de66965365da53c4aad1af010ed16b9dceb
Niels Voss <nvosscode@gmail.com>
2021-03-25 22:48:02 -0700

    Make shutdown script run from correct directory

--- resources/templates/paper-server/shutdown.sh
+++ resources/templates/paper-server/shutdown.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/ec2-user
 screen -S minecraft -p 0 -X stuff 'tellraw @a {"text":"Server will shutdown in 1 minute","color":"red"}^M'
 sleep 30
 screen -S minecraft -p 0 -X stuff 'tellraw @a {"text":"Server will shutdown in 30 seconds","color":"red"}^M'
@@ -7,6 +8,6 @@ screen -S minecraft -p 0 -X stuff 'tellraw @a {"text":"Server will shutdown in 1
 sleep 10
 screen -S minecraft -p 0 -X stuff "stop^M"
 sleep 30
-sh backup.sh
+sh server/backup.sh
 sleep 30
 sudo shutdown -h now
