5818a7e501d8ccfb5b805c513e2bb9d7692de548
Niels Voss <nvosscode@gmail.com>
2021-03-25 19:42:05 -0700

    Give permissions to run shut down script

--- resources/templates/paper-server/prepare.sh
+++ resources/templates/paper-server/prepare.sh
@@ -2,7 +2,11 @@
 # Write data bucket name to file
 mkdir /home/ec2-user/info
 echo $DATA_BUCKET > /home/ec2-user/info/DATA_BUCKET_NAME
+
 # Add cron job to crontab
 (crontab -l 2>/dev/null; echo "0 22 * * * /home/ec2-user/server/backup.sh") | crontab -
 (crontab -l 2>/dev/null; echo "0 */4 * * * /home/ec2-user/server/backup.sh shifting") | crontab -
 sudo chmod +x /home/ec2-user/server/backup.sh
+
+# Give permissions to run shut down script
+sudo chmod +x /home/ec2-user/server/shutdown.sh
