6e9888968509f9a955e7fea4dc4e8350e27f2a7c
Niels Voss <nvosscode@gmail.com>
2021-06-30 11:50:11 -0600

    Allow setting the display name and updating the Table in Server class

--- core/src/main/java/osbourn/cloudcubes/core/server/Server.java
+++ core/src/main/java/osbourn/cloudcubes/core/server/Server.java
@@ -2,6 +2,9 @@ package osbourn.cloudcubes.core.server;
 
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 
 /**
  * Represents a server entry on the DynamoDB database.
@@ -9,6 +12,8 @@ import com.amazonaws.services.dynamodbv2.document.Table;
 public class Server {
     private final Table table;
     private String name;
+
+    private boolean dirty = false;
 
     public final int id;
 
@@ -35,6 +40,49 @@ public class Server {
      */
     public String getName() {
         return name;
+    }
+
+    /**
+     * Sets the display name of the server. Note that this value will not be updated in the database until
+     * {@link #writeChangesToTable()} is called.
+     *
+     * @param name The new display name of the server.
+     */
+    public void setName(String name) {
+        this.name = name;
+        dirty = true;
+    }
+
+    /**
+     * When a value in this class is set via a setter method, it doesn't write the changes out to the database until
+     * {@link #writeChangesToTable()} is called. This object is considered dirty if a value has been set but the
+     * database table has not been updated. This method returns true if the object is dirty and the database needs to
+     * be updated.
+     *
+     * @return True if the database needs to be updated, false otherwise.
+     * @see #writeChangesToTable()
+     */
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    /**
+     * When a value in this class is set via a setter method, it doesn't write teh changes out to the database until
+     * this method is called. (You can check if you need to call this method by running {@link #isDirty()}.)
+     * This method will update the database will the new values that are stored locally in this object.
+     *
+     * @see #isDirty()
+     */
+    public void writeChangesToTable() {
+        String updateExpression = "set Name = :n";
+        ValueMap valueMap = new ValueMap()
+                .withString(":n", this.name);
+        UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("Id", this.id)
+                .withUpdateExpression(updateExpression)
+                .withValueMap(valueMap)
+                .withReturnValues(ReturnValue.UPDATED_NEW);
+        this.table.updateItem(updateItemSpec);
+        dirty = false;
     }
 
     /**
