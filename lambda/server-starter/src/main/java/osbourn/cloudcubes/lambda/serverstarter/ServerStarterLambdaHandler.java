421a0755214079b5c10988e19586f58908b66f32
Niels Voss <nvosscode@gmail.com>
2021-07-25 16:49:39 -0700

    Update references in server starter lambda function to AWS SDK 2.17.4

--- lambda/server-starter/src/main/java/osbourn/cloudcubes/lambda/serverstarter/ServerStarterLambdaHandler.java
+++ lambda/server-starter/src/main/java/osbourn/cloudcubes/lambda/serverstarter/ServerStarterLambdaHandler.java
@@ -1,6 +1,5 @@
 package osbourn.cloudcubes.lambda.serverstarter;
 
-import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -19,11 +18,10 @@ public class ServerStarterLambdaHandler implements RequestHandler<Map<String, St
 
         InfrastructureData infrastructureData = InfrastructureData.fromEnvironment();
         InfrastructureConstructor infrastructureConstructor = new InfrastructureConstructor(infrastructureData);
-        Table serverTable = infrastructureConstructor.getServerTable();
-        Server server = Server.fromId(1, serverTable);
+        Server server = Server.fromId(1, infrastructureConstructor.getDynamoDBClient(), infrastructureData.getServerDataBaseName());
         ServerInstance serverInstance = new ServerInstance(
                 server,
-                infrastructureConstructor.getAmazonEC2(),
+                infrastructureConstructor.ec2Client(),
                 infrastructureData.getServerSubnetIds().get(0),
                 infrastructureData.getServerSecurityGroupName()
         );
