e1d6bdb2ad830081c652a7ea970a371e8264dc80
Niels Voss <nvosscode@gmail.com>
2021-08-06 12:46:01 -0700

    Remove code to install AWS SSM agent from startup.sh
    AWS SSM is already installed on Amazon Linux 2 so this code is redundant

--- resources/server-startup/startup.sh
+++ resources/server-startup/startup.sh
@@ -5,6 +5,3 @@ ec2_instance_metadata_command_result=($(ec2-metadata -i))
 export EC2_ID=${ec2_instance_metadata_command_result[1]}
 ec2_zone_metadata_command_result=($(ec2-metadata -z))
 export EC2_ZONE=${ec2_zone_metadata_command_result[1]}
-
-# Install systems manager (for remote commands)
-sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
