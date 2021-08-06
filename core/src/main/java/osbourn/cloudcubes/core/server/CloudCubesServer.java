0651fdfd55666442d045582782f59b3abea0f70d
Niels Voss <nvosscode@gmail.com>
2021-08-06 13:29:21 -0700

    Start implementing Server interface with CloudCubesServer class

---
+++ core/src/main/java/osbourn/cloudcubes/core/server/CloudCubesServer.java
@@ -0,0 +1,52 @@
+package osbourn.cloudcubes.core.server;
+
+import osbourn.cloudcubes.core.constructs.InfrastructureConstructor;
+import osbourn.cloudcubes.core.constructs.InfrastructureData;
+
+public class CloudCubesServer implements Server {
+    private final int id;
+    private final InfrastructureData infrastructureData;
+    private final ServerTableEntry serverTableEntry;
+    private final ServerInstance serverInstance;
+    private final ServerOptions serverOptions;
+
+    private CloudCubesServer(
+            int id,
+            InfrastructureData infrastructureData,
+            ServerTableEntry serverTableEntry,
+            ServerInstance serverInstance,
+            ServerOptions serverOptions
+    ) {
+        this.id = id;
+        this.infrastructureData = infrastructureData;
+        this.serverTableEntry = serverTableEntry;
+        this.serverInstance = serverInstance;
+        this.serverOptions = serverOptions;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public ServerState getServerState() {
+        return null;
+    }
+
+    public static CloudCubesServer fromId(int id, InfrastructureData infrastructureData) {
+        InfrastructureConstructor infrastructureConstructor = new InfrastructureConstructor(infrastructureData);
+        ServerTableEntry serverTableEntry = ServerTableEntry.fromId(
+                id, infrastructureConstructor.getDynamoDBClient(), infrastructureData.getServerDataBaseName());
+        ServerOptions serverOptions = new ServerOptions(serverTableEntry);
+        ServerInstance serverInstance = new ServerInstance(
+                serverTableEntry,
+                infrastructureConstructor.getEc2Client(),
+                infrastructureData,
+                infrastructureData.getServerInstanceProfileArn(),
+                infrastructureData.getServerSubnetIds().get(0),
+                infrastructureData.getServerSecurityGroupName()
+        );
+        return new CloudCubesServer(id, infrastructureData, serverTableEntry, serverInstance, serverOptions);
+    }
+}
