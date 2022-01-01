d926424e92e5218482fcb48f944010988969bbc3
Niels Voss <nvosscode@gmail.com>
2021-12-31 16:32:13 -0800

    Add documentation to ProvisionalServerState

--- core/src/main/java/osbourn/cloudcubes/core/server/ProvisionalServerState.java
+++ core/src/main/java/osbourn/cloudcubes/core/server/ProvisionalServerState.java
@@ -1,5 +1,10 @@
 package osbourn.cloudcubes.core.server;
 
+/**
+ * The ServerState as it appears in the database. This differs from {@link ServerState} because it contains an UNKNOWN
+ * value, which this will be set to if the server state is unknown. In general, it is necessary to verify the server
+ * state if the state is UNKNOWN.
+ */
 public enum ProvisionalServerState {
     OFFLINE,
     ONLINE,
